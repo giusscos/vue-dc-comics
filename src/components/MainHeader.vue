8ab891fd9cc4ba9a6e7517b672e67df37222f483
giusscos <giu.cosenza08@gmail.com>
2022-09-06 15:49:31 +0200

    Refactoring Nav Links

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -5,65 +5,16 @@
                 <img src="@/assets/img/dc-logo.png" alt="logo-dc" />
             </div>
             <nav class="main_nav">
-                <ul class="nav_list uppercase">
-                    <li class="list_item">
-                        <a class="item_link" href="">
-                            Characters
-                        </a>
-                    </li>
-                    <li class="list_item active">
-                        <a class="item_link" href="">
-                            Comics
-                        </a>
-                    </li>
-                    <li class="list_item">
-                        <a class="item_link" href="">
-                            Movies
-                        </a>
-                    </li>
-                    <li class="list_item">
-                        <a class="item_link" href="">
-                            Tv
-                        </a>
-                    </li>
-                    <li class="list_item">
-                        <a class="item_link" href="">
-                            Games
-                        </a>
-                    </li>
-                    <li class="list_item">
-                        <a class="item_link" href="">
-                            Collectibles
-                        </a>
-                    </li>
-                    <li class="list_item">
-                        <a class="item_link" href="">
-                            Videos
-                        </a>
-                    </li>
-                    <li class="list_item">
-                        <a class="item_link" href="">
-                            Fans
-                        </a>
-                    </li>
-                    <li class="list_item">
-                        <a class="item_link" href="">
-                            News
-                        </a>
-                    </li>
-                    <li class="list_item">
-                        <a class="item_link" href="">
-                            Shop
-                        </a>
-                    </li>
-                </ul>
+                <NavLinks />
             </nav>
         </div>
     </header>
 </template>
 
 <script>
-export default {
+import NavLinks from "./NavLinks.vue";
+export default{
+    components: { NavLinks }
 }
 </script>
 
@@ -77,48 +28,6 @@ header {
 
         .logo_wrapper {
             margin-right: auto;
-        }
-
-        .main_nav {
-
-            .nav_list {
-                gap: 1.5rem;
-                height: 100%;
-                display: flex;
-
-                .list_item {
-                    height: 100%;
-                    display: flex;
-                    position: relative;
-                    align-items: center;
-
-                    &.active {
-                        .item_link {
-                            color: $dc-accent_link;
-
-                            &::after {
-                                content: "";
-                                position: absolute;
-                                bottom: 0;
-                                left: 50%;
-                                width: 100%;
-                                height: 0.5rem;
-                                transform: translateX(-50%);
-                                background-color: $dc-accent_link;
-                            }
-                        }
-                    }
-
-                    .item_link {
-                        font-weight: bold;
-                        padding: 1rem 0;
-
-                        &:hover {
-                            color: $dc-accent_link;
-                        }
-                    }
-                }
-            }
         }
     }
 }
---
+++ src/components/NavLinks.vue
@@ -0,0 +1,112 @@
+<template>
+    <ul class="nav_list uppercase">
+        <li class="list_item" v-for="(navLink, i) in navLinks" :key="`${i}nav`" :class="navLink.currentStatus">
+            <a class="item_link" :href="navLink.href">
+                {{ navLink.text }}
+            </a>
+        </li>
+    </ul>
+</template>
+<script>
+const navLinks = [
+    {
+        href: '#',
+        text: 'Characters',
+        currentStatus: '',
+    },
+    {
+        href: '#',
+        text: 'Comics',
+        currentStatus: 'active',
+    },
+    {
+        href: '#',
+        text: 'Movies',
+        currentStatus: '',
+    },
+    {
+        href: '#',
+        text: 'Tv',
+        currentStatus: '',
+    },
+    {
+        href: '#',
+        text: 'Games',
+        currentStatus: '',
+    },
+    {
+        href: '#',
+        text: 'Collectibles',
+        currentStatus: '',
+    },
+    {
+        href: '#',
+        text: 'Videos',
+        currentStatus: '',
+    },
+    {
+        href: '#',
+        text: 'Fans',
+        currentStatus: '',
+    },
+    {
+        href: '#',
+        text: 'News',
+        currentStatus: '',
+    },
+    {
+        href: '#',
+        text: 'Shop',
+        currentStatus: ''
+    },
+]
+export default {
+    data() {
+        return {
+            navLinks
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import '@/style/variables';
+
+.nav_list {
+    gap: 1.5rem;
+    height: 100%;
+    display: flex;
+
+    .list_item {
+        height: 100%;
+        display: flex;
+        position: relative;
+        align-items: center;
+
+        &.active {
+            .item_link {
+                color: $dc-accent_link;
+
+                &::after {
+                    content: "";
+                    position: absolute;
+                    bottom: 0;
+                    left: 50%;
+                    width: 100%;
+                    height: 0.5rem;
+                    transform: translateX(-50%);
+                    background-color: $dc-accent_link;
+                }
+            }
+        }
+
+        .item_link {
+            font-weight: bold;
+            padding: 1rem 0;
+
+            &:hover {
+                color: $dc-accent_link;
+            }
+        }
+    }
+}
+</style>
