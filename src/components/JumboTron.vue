51fbd98f7aaa66c4d6fe9dd852c92793a96e97a3
giusscos <giu.cosenza08@gmail.com>
2022-09-06 15:03:25 +0200

    Aggiunta Jumbotron

---
+++ src/components/JumboTron.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="jumbo">
+
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+.jumbo {
+    height: 300px;
+    background-image: url("@/assets/img/jumbotron.jpg");
+    background-position: center top;
+    background-size: cover;
+}
+</style>
